1cb27af1bb3bbd23f962fba86aaebc8b887a540e
Adi Stroianu <astroianu530@gmail.com>
2025-01-03 06:16:09 +0200

    Working api kinda

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import requests
 from fastapi.middleware.cors import CORSMiddleware
 import logging
+from dummy_jsons import CARS_LIST, DUMMY_JSON
+from inference import get_state
 
 
 logging.basicConfig(filename="app.log", filemode='a')
@@ -20,4 +23,14 @@ app.add_middleware(
 
 @app.get("/api/appState")
 def get_car_info(carId: Union[str, None] = None):
-    return 
+    return DUMMY_JSON
+
+
+@app.get("/api/getCars")
+def get_cars():
+    return CARS_LIST
+
+
+@app.get("/assets/{filename}")
+def get_file(filename):
+    return FileResponse("assets/" + filename)
